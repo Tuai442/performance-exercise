1f458d4c70837ec940f51152fb95e63bf244cd9c
Tuai442 <tuur.vanholen@outlook.com>
2023-08-27 10:31:55 +0200

    init

---
+++ EscapeFromTheWoodsToRefactor/test/Program.cs
@@ -0,0 +1,13 @@
+﻿using EscapeFromTheWoods.Database.MongoDB;
+using test;
+
+MongoDBWoodRepository repository = new MongoDBWoodRepository("mongodb://localhost:27017");
+
+
+//WoodMongoDBData wood = new WoodMongoDBData(1, 1, 1, 10, 10);
+
+List<WoodMongoDBData> woods = new List<WoodMongoDBData>();
+
+//woods.Add(wood);
+//repository.WriteWoods(woods);
+//Console.WriteLine("opgeslaan");
---
+++ EscapeFromTheWoodsToRefactor/test/TestData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class TestData
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public TestData(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+    }
+}
